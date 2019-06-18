016f6f7adfe8964333c7f759d99a1c9831401e19
Matt Arrick <mattarrick@gmail.com>
2019-06-18 17:23:56 +0000

    Lesson 9: Validating Grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -24,6 +24,13 @@ RSpec.describe GramsController, type: :controller do
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
     end
+
+    it "should properly deal wth validation errors" do
+      post :create, params: { gram: { message: '' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(Gram.count).to eq 0
+    end
   end
+
 
 end
