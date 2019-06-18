71767e51d5c805a4e068d7337e4678bcfd5c0045
Matt Arrick <mattarrick@gmail.com>
2019-06-18 18:19:40 +0000

    Lesson 11: Adding FactoryBot

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -16,11 +16,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should successfully show the new form" do
-      user = User.create(
-        email:                    'fakeuser@gmail.com',
-        password:                 'secretPassword',
-        password_confirmation:    'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       get :new
@@ -33,13 +29,9 @@ RSpec.describe GramsController, type: :controller do
       post :create, params: { gram: { message: "Hello" } }
       expect(response).to redirect_to new_user_session_path
     end
-    
+
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                    'fakeuser@gmail.com',
-        password:                 'secretPassword',
-        password_confirmation:    'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
       
       post :create, params: { gram: { message: 'Hello!' } }
@@ -51,11 +43,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should properly deal wth validation errors" do
-      user = User.create(
-        email:                    'fakeuser@gmail.com',
-        password:                 'secretPassword',
-        password_confirmation:    'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
       
       post :create, params: { gram: { message: '' } }
