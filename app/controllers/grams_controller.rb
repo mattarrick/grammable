aeba7acad5750204bff432c739a0b2dccb657642
Matt Arrick <mattarrick@gmail.com>
2019-06-18 16:53:36 +0000

    Lesson 7: Adding Grams to the Database

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,21 @@
 class GramsController < ApplicationController
-  def index
+  def new
+    @gram = Gram.new
   end
+
+  def index
+
+  end
+
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
+
 end
